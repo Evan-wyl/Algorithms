fd59dfa3e4fa519890b8ae49c0ff69c25b07d309
柯南道尔 <68650142+Evan-wyl@users.noreply.github.com>
2021-10-28 08:31:13 +0800

    Add files via upload

---
+++ RS/deep/DeepFM.py
@@ -0,0 +1,6 @@
+# -*- codeing = utf-8 -*-
+# @Time : 2021/10/28 8:28
+# @Author : Evan_wyl
+# @File : DeepFM.py
+
+
---
+++ RS/deep/xDeepFM.py
@@ -0,0 +1,4 @@
+# -*- codeing = utf-8 -*-
+# @Time : 2021/10/28 8:29
+# @Author : Evan_wyl
+# @File : xDeepFM.py

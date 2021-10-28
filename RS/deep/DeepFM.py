18b57ff67b2146496b66c3b7c51e215236def696
柯南道尔 <68650142+Evan-wyl@users.noreply.github.com>
2021-10-28 12:18:20 +0800

    Delete RS directory

--- RS/deep/DeepFM.py
+++
@@ -1,6 +0,0 @@
-# -*- codeing = utf-8 -*-
-# @Time : 2021/10/28 8:28
-# @Author : Evan_wyl
-# @File : DeepFM.py
-
-
--- RS/deep/xDeepFM.py
+++
@@ -1,4 +0,0 @@
-# -*- codeing = utf-8 -*-
-# @Time : 2021/10/28 8:29
-# @Author : Evan_wyl
-# @File : xDeepFM.py
